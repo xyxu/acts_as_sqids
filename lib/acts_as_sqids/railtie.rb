3967a35afbe461bb72aec2fa785100a1c927fad8
xyxu <37847940@qq.com>
2024-05-30 14:22:22 +0800

    fix: cleanup

--- lib/acts_as_sqids/railtie.rb
+++ lib/acts_as_sqids/railtie.rb
@@ -2,7 +2,7 @@ require 'acts_as_sqids/methods'
 
 module ActsAsSqids
   class Railtie < Rails::Railtie
-    initializer 'acts_as_hashids.include_methods' do
+    initializer 'acts_as_sqids.include_methods' do
       ActiveSupport.on_load :active_record do
         ActiveRecord::Base.send(:include, ActsAsSqids::Methods)
       end
--- spec/acts_as_sqids_spec.rb
+++ spec/acts_as_sqids_spec.rb
@@ -4,7 +4,7 @@ describe ActsAsSqids do
   it 'has a version number' do
     expect(ActsAsSqids::VERSION).not_to be nil
   end
-  it 'defines acts_as_hashids method in ActiveRecord::Base' do
+  it 'defines acts_as_sqids method in ActiveRecord::Base' do
     expect(ActiveRecord::Base).to respond_to(:acts_as_sqids)
   end
 end
